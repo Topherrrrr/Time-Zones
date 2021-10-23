0d442879a42daf3abe1af48cdf1c674f3a93da3f
Topherrrrr <35587956+Topherrrrr@users.noreply.github.com>
2021-10-23 13:45:17 -0500

    Second commit Modified TimeMain to display and update all values for individual popup windows. Modified starter to open all current windows and update them Updated values now show time in other zones Reorganized the main window to be more user friendly

--- Starter.py
+++ Starter.py
@@ -1,8 +1,20 @@
-from random import random
+from datetime import timedelta
+from datetime import datetime
 import storeValues
 import tkinter as tk
 
-popupList=[]
+class timeWidget(object):
+    def __init__(self, modifier, timeChanger, description, label):
+        self.modifier=modifier
+        self.timeChanger=int(timeChanger)
+        self.description=description
+        self.label=label
+    #
+    # def destroyLabel(self):
+    #     self.label.destroy()
+
+widgetList=[]
+
 popup2 = tk.Tk()
 
 
@@ -13,19 +25,31 @@ def openAll():
         popup2 = tk.Tk()
         popup2.geometry("200x100")
         popup2.wm_title("Filler1")
+        description = i[0]
+        timeModifier = i[1][1]
+        symbol = i[1][0]
         label2 = tk.Label(popup2, text=f"{i[1]}: Time zone for {i[0]}")
         label2.place(x=0, y=0)
-        popupList.append(label2)
+        newTimeWidget = timeWidget(symbol, timeModifier, description, label2)
+
+        widgetList.append(newTimeWidget)
         # pool = mp.Pool()
         # p1 = pool.map_async(updateTime(label2))
-        updateTime()
+    updateTime()
 
 def updateTime():
-
-    for i in popupList:
-        r=random()
-        i.config(text=f"R {r}")
-        print(f"R: {r}")
+    now=datetime.now()
+    for i in widgetList:
+        if i.modifier=="-":
+            i.timeChanger*=-1
+        hours=i.timeChanger
+        hoursAdded=timedelta(hours=hours)
+        futureTime=datetime.now()+hoursAdded
+        mins=futureTime.minute
+        hours=futureTime.hour
+        if mins <10:
+            mins="0"+str(mins)
+        i.label.config(text=f"{hours}:{mins}\t{i.description}")
 
     popup2.after(1000,updateTime)
 
--- TimeMain.py
+++ TimeMain.py
@@ -1,28 +1,26 @@
-import time
 import tkinter as tk
-from random import random
 from tkinter import *
-from datetime import date
+from datetime import timedelta
 from datetime import datetime
 import storeValues
-import multiprocessing as mp
 
 root=tk.Tk()
-root.geometry("500x500")
+root.title('Time Zone App')
+root.geometry("500x300")
 
-popupList=[]
+class timeWidget(object):
+    def __init__(self, modifier, timeChanger, description, label):
+        self.modifier=modifier
+        self.timeChanger=int(timeChanger)
+        self.description=description
+        self.label=label
+        self.negative=False
 
-class desktopWidget:
-    def __init__(self, Modifier, Time, Description, Label):
-        self.description=Description
-        self.time=Time
-        self.modifier=Modifier
-        self.label=Label
+    #
+    # def destroyLabel(self):
+    #     self.label.destroy()
 
-    def updateTime(self):
-        nowTime=datetime.now()
-        time=nowTime.strftime("%H:%M")
-        self.label.config(text=f"It is {time} in {self.description}")
+widgetList=[]
 
 #Possible times that could be added to a dropdown, and populating it with numbers 1-23
 timeNums=[]
@@ -38,31 +36,53 @@ def getFields():
     time1=timeDD.get()
     adTime=time2.get()
     storeValues.startSQL(title, adTime, time1)
-
-
+    popup2 = tk.Tk()
+    popup2.geometry("200x100")
+    popup2.wm_title("Filler1")
+    description = title
+    timeModifier = time1
+    symbol = adTime
+    label2 = tk.Label(popup2, text=f" Time zone for {description}")
+    label2.place(x=0, y=0)
+    newTimeWidget = timeWidget(symbol, timeModifier, description, label2)
+    widgetList.append(newTimeWidget)
+    updateTime()
 
 def updateTime():
-
-    for i in popupList:
-        r=random()
-        i.config(text=f"R {r}")
-        print(f"R: {r}")
+    now=datetime.now()
+    for i in widgetList:
+        if i.modifier=="-" and i.negative==False:
+            i.timeChanger*=-1
+            i.negative=True
+        hours=i.timeChanger
+        hoursAdded=timedelta(hours=hours)
+        futureTime=datetime.now()+hoursAdded
+        mins=futureTime.minute
+        hours=futureTime.hour
+        if mins <10:
+            mins="0"+str(mins)
+        i.label.config(text=f"{hours}:{mins}\t{i.description}")
 
     root.after(1000,updateTime)
 
 def openAll():
     print(f"Full List: {storeValues.fetchData()}")
-
+    widgetList.clear()
     for i in storeValues.fetchData():
         popup2=tk.Tk()
         popup2.geometry("200x100")
         popup2.wm_title("Filler1")
+        description=i[0]
+        timeModifier=i[1][1]
+        symbol=i[1][0]
         label2=tk.Label(popup2, text=f"{i[1]}: Time zone for {i[0]}")
         label2.place(x=0, y=0)
-        popupList.append(label2)
+        newTimeWidget=timeWidget(symbol, timeModifier, description,label2)
+
+        widgetList.append(newTimeWidget)
         # pool = mp.Pool()
         # p1 = pool.map_async(updateTime(label2))
-  #  updateTime()
+    updateTime()
 
 
 
@@ -75,33 +95,30 @@ l.pack()
 
 #Writing the main line for time entry
 guideLine=tk.Label(root, text="Setting a time that is                                 hours off from now")
-guideLine.place(x=0,y=50)
+guideLine.place(x=30,y=50)
 
 #Time numbers dropdown
 timeDD=StringVar(root)
 timeGUI=OptionMenu(root, timeDD, *timeNums)
-timeGUI.place(x=160,y=48)
+timeGUI.place(x=190,y=48)
 
 #Adding the + or - sign dropdown
 time2=StringVar(root)
 time2GUI=OptionMenu(root, time2, *timeAds)
-time2GUI.place(x=112,y=48)
+time2GUI.place(x=142,y=48)
 
 #Adding stuff for a description input field
 guide2=tk.Label(root, text="Enter a brief description for this time")
-guide2.place(x=0, y=100)
+guide2.place(x=30, y=100)
 timeDesc=tk.Text(root, height=5, width=52)
-timeDesc.place(x=0, y=120)
+timeDesc.place(x=30, y=120)
 
 
 #A submit button that stores the values placed in the fields
-submitBTN=tk.Button(root, height=5, width=10, text="Submit time", command=lambda:getFields())
-submitBTN.place(x=0, y=190)
+submitBTN=tk.Button(root, height=2, width=10, text="Submit time", command=lambda:getFields())
+submitBTN.place(x=76, y=210)
 
-seeBTN=tk.Button(root, height=5, width=10, text="See time", command=lambda:storeValues.fetchData())
-seeBTN.place(x=0, y=300)
-
-openWindows=tk.Button(root, height=5, width=10, text="Open all", command=lambda:openAll())
-openWindows.place(x=0, y=350)
+openWindows=tk.Button(root, height=2, width=10, text="Open all", command=lambda:openAll())
+openWindows.place(x=280, y=210)
 
 tk.mainloop()
